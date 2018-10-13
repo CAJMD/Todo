2f62520ef9337f017f6163c737165303a9f0e27b
CAJMD <cynthia.nolan@gmail.com>
2018-10-13 21:48:55 +0100

    printing a sorted list

--- Main.py
+++ Main.py
@@ -1,21 +1,22 @@
-TodoList = []
+TodoList = {}
 
 def PrintTodoList():
-    for Todo in TodoList:
-        print (Todo)
+    for Priority, Todo in sorted(TodoList.items()):
+        print (Todo, Priority)
 
 
 def GetTodoTask():
     Todo = input ("What do you want to do now? ")
-    return Todo
+    Priority = input ("Rank this task by importance:")
+    return Todo, Priority
 
 
 def AddTodoTaskToList():
-    TodoList.append(Todo) 
+    TodoList[Priority] = Todo
 
 while True:
     PrintTodoList()
-    Todo = GetTodoTask()
+    Todo, Priority = GetTodoTask()
     if Todo == 'stop':
         break
     else:
